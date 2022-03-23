87e3a4e12b4f77339be1c1f404c3badbd6a904c8
isara-c <isara.c@siametrics.com>
2022-03-23 12:27:48 +0700

    added logout

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -7,12 +7,14 @@ const { COOKIE_NAME } = process.env;
 export default async (_: NextApiRequest, res: NextApiResponse) => {
 	// remove cookie from request header
 	res.setHeader("Set-Cookie", [
+		'WebsiteToken=deleted; Max-Age=0',
+		'AnotherCookieName=deleted; Max-Age=0',
 		serialize(COOKIE_NAME, "", {
 			maxAge: -1,
 			path: "/",
 		}),
 	]);
 
-	res.writeHead(302, { Location: "/" });
+	res.writeHead(302, { Location: "/api/oauth" });
 	res.end();
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,25 @@
 import { GetServerSideProps } from "next";
-import { DiscordUser } from "../../utils/types";
-import { parseUser } from "../../utils/parse-user";
-import axios from 'axios';
-
-const logoutRoute = './api/logout';
-
-const logoutRequest = () => axios.get(logoutRoute);
-
-async function handleLogoutClick() {
-  return await logoutRequest();
-}
+import { DiscordUser } from "../utils/types";
+import { parseUser } from "../utils/parse-user";
+import { useRouter } from 'next/router'
 
 interface Props {
   user: DiscordUser;
 }
 
 export default function Index(props: Props) {
+  const router = useRouter()
   return (
     <h1>
-        Hey, {props.user.username}#{props.user.discriminator} 
-        <h2>
-            ID:{props.user.id}
-            <button onClick={logoutRequest}>
-              {('logout')}
-            </button>
-        </h2>
+      Hey, {props.user.username}#{props.user.discriminator}
+      <h2>
+        ID: {props.user.id}
+        <h3>
+          <button onClick={() => router.push('/api/logout')}>
+            log out
+          </button>
+        </h3>
+      </h2>
     </h1>
   );
 }
